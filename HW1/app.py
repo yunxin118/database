b8b54f5756139de8a056bc20cfa01c84e2270e52
Nebula Lee <144138171+yunxin118@users.noreply.github.com>
2024-10-27 17:28:45 +0800

    Update app.py

--- HW1/app.py
+++ HW1/app.py
@@ -1,1 +1,34 @@
+from flask import Flask, render_template, request, redirect
+import mysql.connector
+from werkzeug.security import generate_password_hash
 
+app = Flask(__name__)
+
+# MySQL connection
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="ruby2302",
+    database="user_data"
+)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/healthmeta', methods=['POST'])
+def healthmeta():
+    username = request.form['username']
+    height = float(request.form['height'])
+    weight = float(request.form['weight'])
+    medicine = request.form['medicine']
+    
+    cursor = db.cursor()
+    sql = "INSERT INTO users (username, height, weight, medicine) VALUES (%s, %s, %s, %s)"
+    val = (username, height, weight, medicine)
+    cursor.execute(sql, val)
+    db.commit()
+    return redirect('/')
+
+if __name__ == "__main__":
+    app.run(debug=True)
